8b1e3fc7cc9b25eea2c92a35434958d3f4f9bc66
Malintha Fernando <malinthafe@wso2.com>
2016-08-19 10:46:15 +0530

    Adding yaml parsing with snakeyaml

--- src/main/java/org/wso2/yaml/InstanceValidator.java
+++ src/main/java/org/wso2/yaml/InstanceValidator.java
@@ -1,15 +1,26 @@
 package org.wso2.yaml;
 
-import org.ho.yaml.YamlDecoder;
+import org.yaml.snakeyaml.Yaml;
 
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.Map;
 
-/**
- * Created by malintha on 8/18/16.
+/*
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 public class InstanceValidator {
 
     public static void main(String[] args) throws EOFException {
@@ -20,19 +31,20 @@ public class InstanceValidator {
             e.printStackTrace();
         }
         if(yamlResource == null) {
-            System.out.println("####Yaml Input stream is null####");
+            System.out.println("####Yaml input stream is null####");
             return;
         }
 
-        YamlDecoder yamlDecoder = new YamlDecoder(yamlResource);
-        Object obj = yamlDecoder.readObject();
-        System.out.println(obj);
+        Yaml yaml = new Yaml();
+        Map<String, Map<String, String>> rxtConfig = yaml.loadAs(yamlResource, Map.class);
+        Map<String, String> nameElem = rxtConfig.get("name");
+        System.out.println(nameElem.toString());
 
 
     }
 
     private static FileInputStream getYamlResource() throws FileNotFoundException {
-        FileInputStream yamlIs = new FileInputStream(new File("/home/malintha/Desktop/MetaC5/RXT-Model-YAML/src/main/resources/soapservice.yml"));
+        FileInputStream yamlIs = new FileInputStream(new File("/home/malintha/Desktop/MetaC5/RXT-Model-YAML/src/main/resources/type.yml"));
         return yamlIs;
     }
 
